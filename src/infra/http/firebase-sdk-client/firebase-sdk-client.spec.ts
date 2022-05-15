1d6b8176877912bea7954c37999a195b2f68dba4
Renan Gianni de Melo <renan.melo@MacBook-Pro-de-Compass.local>
2022-05-15 12:16:05 -0300

    feat: add firebase infra

---
+++ src/infra/http/firebase-sdk-client/firebase-sdk-client.spec.ts
@@ -0,0 +1,9 @@
+import { mockPostRequest } from "../../../data/test/"
+
+describe('FirebaseSdkClient', () => {
+    describe('post', () => {
+        it('Should call firebase.post with correct values', async () => {
+            const request = mockPostRequest()
+        })
+    })
+})
---
+++ src/infra/http/firebase-sdk-client/firebase-sdk-client.ts
@@ -0,0 +1,15 @@
+import { HttpPostClient, HttpPostParams } from "../../../data/protocols/http";
+
+
+export function firebaseSdkClient(): HttpPostClient {
+    return {
+        post(params: HttpPostParams) {
+            try {
+
+                return Promise.resolve()
+            } catch(err)   {
+                
+            }
+        }
+    }
+}

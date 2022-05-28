a7f56b1bd40558f26073fd43c2cfd03bcdc05ec7
Milena Hikari <milenahikari@MacBook-Air-de-Milena.local>
2022-05-28 18:42:52 -0300

    feat: add test add button component

--- src/presentations/components/AddButton/index.tsx
+++ src/presentations/components/AddButton/index.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { useTheme } from "styled-components";
+
 import { Entypo } from "@expo/vector-icons";
 
 import * as S from "./styles";
 
 export function AddButton() {
-  const theme = useTheme();
-
   return (
-    <S.Container>
-      <Entypo name="plus" size={25} color={theme.colors.backgroundLight} />
+    <S.Container testID="button">
+      <Entypo testID="icon" name="plus" size={25} color="#ffffff" />
     </S.Container>
   );
 }
---
+++ src/presentations/page/addSheet/add-sheet.spec.tsx
@@ -0,0 +1,8 @@
+import { render } from "../../../utils/test.utils";
+
+import { AddSheetPage } from ".";
+describe("AddSheetPage", () => {
+  it("should render an add button", () => {
+    // const { getByTestId } = render(<AddSheetPage loadSheet={} addSheet={} />);
+  });
+});
